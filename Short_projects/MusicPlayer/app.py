f96e7737a8a8e244d5655578ca92e4d33d7e7c7b
Tahir2022 <abbasovtahir617@gmail.com>
2022-06-14 00:08:20 +0400

    m

---
+++ Short_projects/MusicPlayer/app.py
@@ -0,0 +1,13 @@
+from pygame import mixer
+mixer.init()
+mixer.music.load('filename.mp3')
+mixer.music.play()
+
+
+
+
+
+
+
+
+
---
+++ turtle/run.py
@@ -0,0 +1,43 @@
+import turtle
+t = turtle.Pen()
+#Rectangle
+
+#t.forward(100)
+#t.left(90)
+#t.forward(300)
+#t.left(90)
+#t.forward(100)
+#t.left(90)
+#t.forward(300)
+#Triangle
+
+#t.forward(100)
+#t.left(135)
+#t.forward(200)
+#t.left(90)
+#t.forward(200)
+#t.left(135)
+#t.forward(200)
+
+#A Box Without Corners
+#t.forward(100)
+#t.left(90)
+#t.up()
+#t.forward(100)
+#t.left(90)
+#t.forward(100)
+
+while True:
+	t.forward(100)
+	t.left(135)
+	t.forward(200)
+	t.left(90)
+	t.forward(200)
+	t.left(135)
+	t.forward(200)
+
+
+
+
+
+
